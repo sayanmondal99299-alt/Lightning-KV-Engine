c517f79deebc414b4347e9bd185b4be1b986e3a6
sayanmondal99299-alt <sayanmondal99299@gmail.com>
2026-01-05 13:58:52 +0530

    Initial code for Lightning-KV-Engine।

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+int main() {
+    std::cout << "Lightning-KV-Engine Initialized..." << std::endl;
+    std::cout << "Status: High-Performance Lock-Free System Ready." << std::endl;
+    return 0;
+}
